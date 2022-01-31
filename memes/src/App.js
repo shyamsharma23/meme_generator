1f93184107efd5b50ef26b77ae10500726479e2a
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2022-01-31 12:38:28 -0600

    First commit

---
+++ memes/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header.js';
+import Meme from './components/Meme.js';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ memes/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className='nav'>
+    <div className='logo'>
+      <img src='./images/Troll Face.png'/>
+      <h1>Meme Generator</h1>
+      </div>
+      <div className='text'>
+    <h3>React Course Project-3</h3>
+    </div>
+    
+      
+  </div>
+};
+
+export default Header;
